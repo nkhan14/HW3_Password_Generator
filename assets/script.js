9cc4550e9dac395f6efb8e192c6bd72766119c5f
Nabiha Khan <nabiha@northwestern.edu>
2021-04-19 00:55:48 -0500

    Trying to resolve comma issue

--- assets/script.js
+++ assets/script.js
@@ -83,15 +83,15 @@ function generatePassword() {
     myArray2.push(random(loweCase));
   }
 
-  for (let i = 0; i - pwChoices.length; i++) {
-    let myArray1 = random(myArray1);
-    result.push(myArray1);
-  }
+  // for (let i = 0; i - pwChoices.length; i++) {
+  //   let myArray1 = random(myArray1);
+  //   result.push(myArray1);
+  // }
   for (let i = 0; i - myArray2.length; i++) {
     result[i] = myArray2;
   }
 
-  return result.join("");
+  return result.join('_');
 }
 
 let generateBtn = document.querySelector("#generate");
